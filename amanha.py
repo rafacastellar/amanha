3a464e32e38ec71b6a0d22dd15e89593421727aa
Rafael Castellar <rafael.castellar@aluno.impacta.edu.br>
2026-03-20 07:43:30 -0400

    upload do projeto

---
+++ amanha.py
@@ -0,0 +1,12 @@
+# Crie um programa que receba como entrada uma data
+# DD/MM/AAAA e imprima a data do dia seguinte.
+#obs: caso a data seja invalida, o programa devera alertar o usuario o usuario e encerrar a execução.
+
+def main():
+    d, m, a = input('data?')
+
+
+
+
+
+    main()
